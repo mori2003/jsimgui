4b8717d4b1ce40f77fb1c32dec8616dd358b2921
Mori <139063007+mori2003@users.noreply.github.com>
2025-08-05 22:46:16 +0200

    feat(build)!: rewrite build script

--- src/templates/ts/impl.ts
+++ src/templates/ts/impl.ts
@@ -47,7 +47,6 @@ export const ImGuiImplWGPU = {
     },
 };
 
-
 const MOUSE_BUTTON_MAP = {
     0: ImGui.MouseButton.Left,
     1: ImGui.MouseButton.Middle,
@@ -79,101 +78,101 @@ const KEYBOARD_MAP = {
     "8": ImGui.Key._8,
     "9": ImGui.Key._9,
 
-    "Numpad0": ImGui.Key._Keypad0,
-    "Numpad1": ImGui.Key._Keypad1,
-    "Numpad2": ImGui.Key._Keypad2,
-    "Numpad3": ImGui.Key._Keypad3,
-    "Numpad4": ImGui.Key._Keypad4,
-    "Numpad5": ImGui.Key._Keypad5,
-    "Numpad6": ImGui.Key._Keypad6,
-    "Numpad7": ImGui.Key._Keypad7,
-    "Numpad8": ImGui.Key._Keypad8,
-    "Numpad9": ImGui.Key._Keypad9,
-    "NumpadDecimal": ImGui.Key._KeypadDecimal,
-    "NumpadDivide": ImGui.Key._KeypadDivide,
-    "NumpadMultiply": ImGui.Key._KeypadMultiply,
-    "NumpadSubtract": ImGui.Key._KeypadSubtract,
-    "NumpadAdd": ImGui.Key._KeypadAdd,
-    "NumpadEnter": ImGui.Key._KeypadEnter,
-    "NumpadEqual": ImGui.Key._KeypadEqual,
+    Numpad0: ImGui.Key._Keypad0,
+    Numpad1: ImGui.Key._Keypad1,
+    Numpad2: ImGui.Key._Keypad2,
+    Numpad3: ImGui.Key._Keypad3,
+    Numpad4: ImGui.Key._Keypad4,
+    Numpad5: ImGui.Key._Keypad5,
+    Numpad6: ImGui.Key._Keypad6,
+    Numpad7: ImGui.Key._Keypad7,
+    Numpad8: ImGui.Key._Keypad8,
+    Numpad9: ImGui.Key._Keypad9,
+    NumpadDecimal: ImGui.Key._KeypadDecimal,
+    NumpadDivide: ImGui.Key._KeypadDivide,
+    NumpadMultiply: ImGui.Key._KeypadMultiply,
+    NumpadSubtract: ImGui.Key._KeypadSubtract,
+    NumpadAdd: ImGui.Key._KeypadAdd,
+    NumpadEnter: ImGui.Key._KeypadEnter,
+    NumpadEqual: ImGui.Key._KeypadEqual,
 
-    "F1": ImGui.Key._F1,
-    "F2": ImGui.Key._F2,
-    "F3": ImGui.Key._F3,
-    "F4": ImGui.Key._F4,
-    "F5": ImGui.Key._F5,
-    "F6": ImGui.Key._F6,
-    "F7": ImGui.Key._F7,
-    "F8": ImGui.Key._F8,
-    "F9": ImGui.Key._F9,
-    "F10": ImGui.Key._F10,
-    "F11": ImGui.Key._F11,
-    "F12": ImGui.Key._F12,
-    "F13": ImGui.Key._F13,
-    "F14": ImGui.Key._F14,
-    "F15": ImGui.Key._F15,
-    "F16": ImGui.Key._F16,
-    "F17": ImGui.Key._F17,
-    "F18": ImGui.Key._F18,
-    "F19": ImGui.Key._F19,
-    "F20": ImGui.Key._F20,
-    "F21": ImGui.Key._F21,
-    "F22": ImGui.Key._F22,
-    "F23": ImGui.Key._F23,
-    "F24": ImGui.Key._F24,
+    F1: ImGui.Key._F1,
+    F2: ImGui.Key._F2,
+    F3: ImGui.Key._F3,
+    F4: ImGui.Key._F4,
+    F5: ImGui.Key._F5,
+    F6: ImGui.Key._F6,
+    F7: ImGui.Key._F7,
+    F8: ImGui.Key._F8,
+    F9: ImGui.Key._F9,
+    F10: ImGui.Key._F10,
+    F11: ImGui.Key._F11,
+    F12: ImGui.Key._F12,
+    F13: ImGui.Key._F13,
+    F14: ImGui.Key._F14,
+    F15: ImGui.Key._F15,
+    F16: ImGui.Key._F16,
+    F17: ImGui.Key._F17,
+    F18: ImGui.Key._F18,
+    F19: ImGui.Key._F19,
+    F20: ImGui.Key._F20,
+    F21: ImGui.Key._F21,
+    F22: ImGui.Key._F22,
+    F23: ImGui.Key._F23,
+    F24: ImGui.Key._F24,
 
-    "a": ImGui.Key._A,
-    "b": ImGui.Key._B,
-    "c": ImGui.Key._C,
-    "d": ImGui.Key._D,
-    "e": ImGui.Key._E,
-    "f": ImGui.Key._F,
-    "g": ImGui.Key._G,
-    "h": ImGui.Key._H,
-    "i": ImGui.Key._I,
-    "j": ImGui.Key._J,
-    "k": ImGui.Key._K,
-    "l": ImGui.Key._L,
-    "m": ImGui.Key._M,
-    "n": ImGui.Key._N,
-    "o": ImGui.Key._O,
-    "p": ImGui.Key._P,
-    "q": ImGui.Key._Q,
-    "r": ImGui.Key._R,
-    "s": ImGui.Key._S,
-    "t": ImGui.Key._T,
-    "u": ImGui.Key._U,
-    "v": ImGui.Key._V,
-    "w": ImGui.Key._W,
-    "x": ImGui.Key._X,
-    "y": ImGui.Key._Y,
-    "z": ImGui.Key._Z,
-    "A": ImGui.Key._A,
-    "B": ImGui.Key._B,
-    "C": ImGui.Key._C,
-    "D": ImGui.Key._D,
-    "E": ImGui.Key._E,
-    "F": ImGui.Key._F,
-    "G": ImGui.Key._G,
-    "H": ImGui.Key._H,
-    "I": ImGui.Key._I,
-    "J": ImGui.Key._J,
-    "K": ImGui.Key._K,
-    "L": ImGui.Key._L,
-    "M": ImGui.Key._M,
-    "N": ImGui.Key._N,
-    "O": ImGui.Key._O,
-    "P": ImGui.Key._P,
-    "Q": ImGui.Key._Q,
-    "R": ImGui.Key._R,
-    "S": ImGui.Key._S,
-    "T": ImGui.Key._T,
-    "U": ImGui.Key._U,
-    "V": ImGui.Key._V,
-    "W": ImGui.Key._W,
-    "X": ImGui.Key._X,
-    "Y": ImGui.Key._Y,
-    "Z": ImGui.Key._Z,
+    a: ImGui.Key._A,
+    b: ImGui.Key._B,
+    c: ImGui.Key._C,
+    d: ImGui.Key._D,
+    e: ImGui.Key._E,
+    f: ImGui.Key._F,
+    g: ImGui.Key._G,
+    h: ImGui.Key._H,
+    i: ImGui.Key._I,
+    j: ImGui.Key._J,
+    k: ImGui.Key._K,
+    l: ImGui.Key._L,
+    m: ImGui.Key._M,
+    n: ImGui.Key._N,
+    o: ImGui.Key._O,
+    p: ImGui.Key._P,
+    q: ImGui.Key._Q,
+    r: ImGui.Key._R,
+    s: ImGui.Key._S,
+    t: ImGui.Key._T,
+    u: ImGui.Key._U,
+    v: ImGui.Key._V,
+    w: ImGui.Key._W,
+    x: ImGui.Key._X,
+    y: ImGui.Key._Y,
+    z: ImGui.Key._Z,
+    A: ImGui.Key._A,
+    B: ImGui.Key._B,
+    C: ImGui.Key._C,
+    D: ImGui.Key._D,
+    E: ImGui.Key._E,
+    F: ImGui.Key._F,
+    G: ImGui.Key._G,
+    H: ImGui.Key._H,
+    I: ImGui.Key._I,
+    J: ImGui.Key._J,
+    K: ImGui.Key._K,
+    L: ImGui.Key._L,
+    M: ImGui.Key._M,
+    N: ImGui.Key._N,
+    O: ImGui.Key._O,
+    P: ImGui.Key._P,
+    Q: ImGui.Key._Q,
+    R: ImGui.Key._R,
+    S: ImGui.Key._S,
+    T: ImGui.Key._T,
+    U: ImGui.Key._U,
+    V: ImGui.Key._V,
+    W: ImGui.Key._W,
+    X: ImGui.Key._X,
+    Y: ImGui.Key._Y,
+    Z: ImGui.Key._Z,
     "'": ImGui.Key._Apostrophe,
     ",": ImGui.Key._Comma,
     "-": ImGui.Key._Minus,
@@ -186,41 +185,40 @@ const KEYBOARD_MAP = {
     "]": ImGui.Key._RightBracket,
     "`": ImGui.Key._GraveAccent,
 
-    "CapsLock": ImGui.Key._CapsLock,
-    "ScrollLock": ImGui.Key._ScrollLock,
-    "NumLock": ImGui.Key._NumLock,
-    "PrintScreen": ImGui.Key._PrintScreen,
-    "Pause": ImGui.Key._Pause,
+    CapsLock: ImGui.Key._CapsLock,
+    ScrollLock: ImGui.Key._ScrollLock,
+    NumLock: ImGui.Key._NumLock,
+    PrintScreen: ImGui.Key._PrintScreen,
+    Pause: ImGui.Key._Pause,
 
-    "Tab": ImGui.Key._Tab,
-    "ArrowLeft": ImGui.Key._LeftArrow,
-    "ArrowRight": ImGui.Key._RightArrow,
-    "ArrowUp": ImGui.Key._UpArrow,
-    "ArrowDown": ImGui.Key._DownArrow,
-    "PageUp": ImGui.Key._PageUp,
-    "PageDown": ImGui.Key._PageDown,
-    "Home": ImGui.Key._Home,
-    "End": ImGui.Key._End,
-    "Insert": ImGui.Key._Insert,
-    "Delete": ImGui.Key._Delete,
-    "Backspace": ImGui.Key._Backspace,
+    Tab: ImGui.Key._Tab,
+    ArrowLeft: ImGui.Key._LeftArrow,
+    ArrowRight: ImGui.Key._RightArrow,
+    ArrowUp: ImGui.Key._UpArrow,
+    ArrowDown: ImGui.Key._DownArrow,
+    PageUp: ImGui.Key._PageUp,
+    PageDown: ImGui.Key._PageDown,
+    Home: ImGui.Key._Home,
+    End: ImGui.Key._End,
+    Insert: ImGui.Key._Insert,
+    Delete: ImGui.Key._Delete,
+    Backspace: ImGui.Key._Backspace,
     " ": ImGui.Key._Space,
-    "Enter": ImGui.Key._Enter,
-    "Escape": ImGui.Key._Escape,
+    Enter: ImGui.Key._Enter,
+    Escape: ImGui.Key._Escape,
 
-    "Control": ImGui.Key._LeftCtrl,
-    "Shift": ImGui.Key._LeftShift,
-    "Alt": ImGui.Key._LeftAlt,
-    "Super": ImGui.Key._LeftSuper,
+    Control: ImGui.Key._LeftCtrl,
+    Shift: ImGui.Key._LeftShift,
+    Alt: ImGui.Key._LeftAlt,
+    Super: ImGui.Key._LeftSuper,
 } as const;
 
 const KEYBOARD_MODIFIER_MAP = {
-    "Control": ImGui.Key.ImGuiMod_Ctrl,
-    "Shift": ImGui.Key.ImGuiMod_Shift,
-    "Alt": ImGui.Key.ImGuiMod_Alt,
-    "Super": ImGui.Key.ImGuiMod_Super,
+    Control: ImGui.Key.ImGuiMod_Ctrl,
+    Shift: ImGui.Key.ImGuiMod_Shift,
+    Alt: ImGui.Key.ImGuiMod_Alt,
+    Super: ImGui.Key.ImGuiMod_Super,
 } as const;
-
 
 const handleKeyboardEvent = (event: KeyboardEvent, keyDown: boolean, io: ImGuiIO) => {
     io.AddKeyEvent(KEYBOARD_MAP[event.key as keyof typeof KEYBOARD_MAP], keyDown);
@@ -233,8 +231,7 @@ const handleKeyboardEvent = (event: KeyboardEvent, keyDown: boolean, io: ImGuiIO
     if (event.key.length === 1 && keyDown) {
         io.AddInputCharactersUTF8(event.key);
     }
-}
-
+};
 
 /**
  * Sets up canvas size and resize handling.
@@ -266,7 +263,7 @@ function setupCanvasIO(canvas: HTMLCanvasElement) {
  */
 function setupMouseIO(canvas: HTMLCanvasElement) {
     const io = ImGui.GetIO();
-    const scrollSpeed = 0.01
+    const scrollSpeed = 0.01;
 
     canvas.addEventListener("mousemove", (e) => {
         const rect = canvas.getBoundingClientRect();
@@ -319,7 +316,7 @@ function setupTouchIO(canvas: HTMLCanvasElement) {
 
             const currentPos = {
                 x: (touch1.clientX + touch2.clientX) / 2,
-                y: (touch1.clientY + touch2.clientY) / 2
+                y: (touch1.clientY + touch2.clientY) / 2,
             };
 
             if (lastPos.x > 0 && lastPos.y > 0) {
@@ -330,14 +327,14 @@ function setupTouchIO(canvas: HTMLCanvasElement) {
 
             lastPos = currentPos;
             return;
-    }
+        }
 
         lastPos = { x: 0, y: 0 };
         const touch = event.touches[0];
 
         if (touch) {
             io.AddMousePosEvent(touch.clientX - rect.left, touch.clientY - rect.top);
-    }
+        }
 
         if (typeof isButtonDown === "boolean") {
             io.AddMouseButtonEvent(ImGui.MouseButton.Left, isButtonDown);
@@ -368,18 +365,17 @@ function setupTouchIO(canvas: HTMLCanvasElement) {
             input.addEventListener("blur", blurHandler);
             input.addEventListener("keydown", keyDownHandler);
             input.addEventListener("keyup", (e) => {
-                keyUpHandler(e)
+                keyUpHandler(e);
 
                 // Exits single-line input fields when pressing Enter.
                 if (!io.WantTextInput) {
                     blurHandler();
                 }
             });
-        }
-        else {
+        } else {
             blurHandler();
         }
-    }
+    };
 
     canvas.addEventListener("touchstart", (e) => handleTouchEvent(e, true));
     canvas.addEventListener("touchmove", (e) => handleTouchEvent(e));
@@ -414,75 +410,107 @@ function setupBrowserIO(canvas: HTMLCanvasElement) {
     setupTouchIO(canvas);
 }
 
+/**
+ * Initialization options for Jsimgui.
+ */
+interface InitOptions {
+    canvas: HTMLCanvasElement;
+    device?: GPUDevice;
+    backend?: "webgl" | "webgl2" | "webgpu";
+    fontLoader?: "truetype" | "freetype";
+    useDemos?: boolean;
+}
+
+let beginRenderFn: () => void = () => {};
+let endRenderFn: (passEncoder?: GPURenderPassEncoder) => void = () => {};
+
 /** Web implementation of Jsimgui. */
 export const ImGuiImplWeb = {
-    /** Initialize Dear ImGui with WebGL2 Backend on the given canvas. */
-    async InitWebGL(canvas: HTMLCanvasElement): Promise<void> {
-        await Mod.init();
-        Mod.export.FS.mount(Mod.export.MEMFS, { root: "." }, ".");
+    /** Begin a new ImGui frame. Call this at the beginning of your render loop. */
+    BeginRender() {
+        beginRenderFn();
+        ImGui.NewFrame();
+    },
 
-        const canvasContext = canvas.getContext("webgl2");
+    /** End the current ImGui frame. Call this at the end of your render loop. */
+    EndRender(passEncoder?: GPURenderPassEncoder) {
+        ImGui.Render();
+        endRenderFn(passEncoder);
+    },
 
-        if (!canvasContext) {
-            throw new Error("Failed to create WebGL2 context.");
+    /**
+     * Initialize Dear ImGui with the specified options.
+     */
+    async Init(options: InitOptions) {
+        const { canvas, device, backend, fontLoader = "truetype", useDemos = false } = options;
+
+        const inferredBackend = device
+            ? "webgpu"
+            : (() => {
+                  const ctx = canvas.getContext("webgl2") || canvas.getContext("webgl");
+                  if (ctx instanceof WebGL2RenderingContext) return "webgl2";
+                  if (ctx instanceof WebGLRenderingContext) return "webgl";
+                  return null;
+              })();
+
+
+        if (!backend && !inferredBackend) {
+            throw new Error("Unknown backend.");
         }
 
-        ImGui.CreateContext();
-        setupBrowserIO(canvas);
+        const backendName = backend ?? inferredBackend;
+        const fontLoaderShort = fontLoader === "truetype" ? "tt" : "ft";
+        const loaderPath = `./${backendName}/jsimgui-${backendName}-${fontLoaderShort}${useDemos ? "-demos" : ""}.js`;
 
-        const handle = Mod.export.GL.registerContext(
-            canvasContext,
-            canvasContext.getContextAttributes(),
-        ) as number;
-        Mod.export.GL.makeContextCurrent(handle);
-
-        ImGuiImplOpenGL3.Init();
-    },
-
-    /** Initialize Dear ImGui with WebGPU Backend on the given canvas. */
-    async InitWebGPU(canvas: HTMLCanvasElement, device: GPUDevice) {
-        await Mod.init();
+        await Mod.init(loaderPath);
         Mod.export.FS.mount(Mod.export.MEMFS, { root: "." }, ".");
 
         ImGui.CreateContext();
         setupBrowserIO(canvas);
 
-        Mod.export.preinitializedWebGPUDevice = device;
+        if (backendName === "webgpu") {
+            Mod.export.preinitializedWebGPUDevice = device;
+            ImGuiImplWGPU.Init();
 
-        ImGuiImplWGPU.Init();
-    },
+            beginRenderFn = () => {
+                ImGuiImplWGPU.NewFrame();
+            };
+            endRenderFn = (passEncoder?: GPURenderPassEncoder) => {
+                ImGuiImplWGPU.RenderDrawData(
+                    ImGui.GetDrawData(),
+                    passEncoder as GPURenderPassEncoder,
+                );
+            };
+            return;
+        }
 
-    /** Begin a new ImGui WebGL frame. Call this at the beginning of your render loop. */
-    BeginRenderWebGL(): void {
-        ImGuiImplOpenGL3.NewFrame();
-        ImGui.NewFrame();
-    },
+        if (backendName === "webgl" || backendName === "webgl2") {
+            const ctx = canvas.getContext(backendName) as
+                | WebGLRenderingContext
+                | WebGL2RenderingContext;
+            if (!ctx) throw new Error(`Failed to create ${backendName.toUpperCase()} context`);
 
-    /** Begin a new ImGui WebGPU frame. Call this at the beginning of your render loop. */
-    BeginRenderWebGPU(): void {
-        ImGuiImplWGPU.NewFrame();
-        ImGui.NewFrame();
-    },
+            const handle = Mod.export.GL.registerContext(ctx, ctx.getContextAttributes()) as number;
+            Mod.export.GL.makeContextCurrent(handle);
+            ImGuiImplOpenGL3.Init();
 
-    /** End the current ImGui WebGL frame. Call this at the end of your render loop. */
-    EndRenderWebGL(): void {
-        ImGui.Render();
-        ImGuiImplOpenGL3.RenderDrawData(ImGui.GetDrawData());
-    },
-
-    /** End the current ImGui WebGPU frame. Call this before passEncoder.end(). */
-    EndRenderWebGPU(passEncoder: GPURenderPassEncoder): void {
-        ImGui.Render();
-        ImGuiImplWGPU.RenderDrawData(ImGui.GetDrawData(), passEncoder);
+            beginRenderFn = () => {
+                ImGuiImplOpenGL3.NewFrame();
+            };
+            endRenderFn = () => {
+                ImGuiImplOpenGL3.RenderDrawData(ImGui.GetDrawData());
+            };
+            return;
+        }
     },
 
     /** Load an image to be used on a WebGL canvas. Returns the texture id. */
     LoadImageWebGL(canvas: HTMLCanvasElement, image: HTMLImageElement): Promise<ImTextureID> {
         return new Promise((resolve, reject) => {
             image.onload = () => {
-                const gl = canvas.getContext("webgl2");
+                const gl = canvas.getContext("webgl2") || canvas.getContext("webgl");
                 if (!gl) {
-                    throw new Error("Failed to create WebGL2 context.");
+                    throw new Error("Failed to create WebGL/WebGL2 context.");
                 }
 
                 const texture = gl.createTexture();
@@ -509,7 +537,10 @@ export const ImGuiImplWeb = {
         return new Promise((resolve, reject) => {
             image.onload = () => {
                 const textureDescriptor: GPUTextureDescriptor = {
-                    usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT,
+                    usage:
+                        GPUTextureUsage.COPY_DST |
+                        GPUTextureUsage.TEXTURE_BINDING |
+                        GPUTextureUsage.RENDER_ATTACHMENT,
                     dimension: "2d",
                     size: {
                         width: image.width,
@@ -538,7 +569,11 @@ export const ImGuiImplWeb = {
                     depthOrArrayLayers: 1,
                 };
 
-                device.queue.copyExternalImageToTexture({ source: image }, textureDestination, copySize);
+                device.queue.copyExternalImageToTexture(
+                    { source: image },
+                    textureDestination,
+                    copySize,
+                );
 
                 const textureViewDescriptor: GPUTextureViewDescriptor = {
                     format: "rgba8unorm",
@@ -561,5 +596,37 @@ export const ImGuiImplWeb = {
                 reject(error);
             };
         });
+    },
+
+    GetMemoryInfo(): {
+        heap: {
+            size: number;
+            max: number;
+            sbrk_ptr: number;
+        };
+        mall: {
+            arena: number;
+            ordblks: number;
+            smblks: number;
+            hblks: number;
+            hblkhd: number;
+            usmblks: number;
+            fsmblks: number;
+            uordblks: number;
+            fordblks: number;
+            keepcost: number;
+        };
+        stack: {
+            base: number;
+            end: number;
+            current: number;
+            free: number;
+        };
+    } {
+        return {
+            heap: Mod.export.get_wasm_heap_info(),
+            mall: Mod.export.get_wasm_mall_info(),
+            stack: Mod.export.get_wasm_stack_info(),
+        };
     },
 };
