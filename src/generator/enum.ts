109f3129adfe8e83a1ab5898a99adc7163eb2093
Mori <139063007+mori2003@users.noreply.github.com>
2025-09-15 16:43:22 +0200

    chore(generator): refactor enum generation

--- src/generator/enum.ts
+++ src/generator/enum.ts
@@ -1,35 +1,51 @@
 import { generateJsDocComment } from "./comment.ts";
-import type { ImGuiData, ImGuiEnum } from "./interface.ts";
+import type { ImGuiEnum } from "./interface.ts";
+import type { GeneratorContext } from "./main";
 
-/** Trims the field name to remove the enum name prefix. */
-function trimFieldName(fieldName: string, enumName: string): string {
-    return fieldName.startsWith(enumName) ? fieldName.slice(enumName.length) : fieldName;
-}
+/**
+ * Trims the field name to remove the enum name prefix.
+ * @param field - The name of the field to trim.
+ * @param enumName - The name of the enum to trim.
+ * @returns The trimmed field name.
+ */
+const trimFieldName = (field: string, enumName: string): string => {
+    return field.startsWith(enumName) ? field.slice(enumName.length) : field;
+};
 
-/** Trims the enum name to remove the "ImGui" prefix and "_" suffix. */
-function trimEnumName(enumName: string): string {
+/**
+ * Trims the enum name to remove the "ImGui" prefix and "_" suffix.
+ * @param enumName - The name of the enum to trim.
+ * @returns The trimmed enum name.
+ */
+const trimEnumName = (enumName: string): string => {
     const trimmed = enumName.endsWith("_") ? enumName.slice(0, -1) : enumName;
     return trimmed.startsWith("ImGui") ? trimmed.slice(5) : trimmed;
-}
+};
 
-/** Generates TypeScript code for an enum. */
-function getEnumCode(enumData: ImGuiEnum): string {
-    const trimmedName = trimEnumName(enumData.name);
-    const enumComment = generateJsDocComment(enumData);
+/**
+ * Generates TypeScript code for the enums and flags.
+ * @param ctx - The generator context.
+ * @returns The TypeScript code for the enums and flags properties. This will be placed
+ * in the ImGui object.
+ */
+export const tsGenerateEnums = (ctx: GeneratorContext): string => {
+    return ctx.data.enums
+        .map((enumData: ImGuiEnum) => {
+            // TODO: enable exclusion via config file.
+            // if (shouldExclude(ctx, enumData, "enums")) {
+            //     return "";
+            // }
 
-    const elementCode = enumData.elements.map((element) => {
-        const fieldName = trimFieldName(element.name, enumData.name);
-        const fieldComment = generateJsDocComment(element);
+            const name = trimEnumName(enumData.name);
+            const comment = generateJsDocComment(enumData);
 
-        return [fieldComment, `        ${fieldName}: ${element.value},\n`].join("");
-    });
+            const fields = enumData.elements.map((element) => {
+                const fieldName = trimFieldName(element.name, enumData.name);
+                const fieldComment = generateJsDocComment(element);
+                return `${fieldComment}${fieldName}: ${element.value},\n`;
+            });
 
-    return [enumComment, `    ${trimmedName}: {\n`, ...elementCode, "    },\n", "\n"].join("");
-}
-
-/** Generates TypeScript code for the enums and flags in the ImGui data. */
-export function generateEnumsTs(jsonData: ImGuiData): string {
-    const enumCode = jsonData.enums.map((enumData) => getEnumCode(enumData)).join("");
-
-    return ["    /* Enums/Flags */\n", "\n", ...enumCode].join("");
-}
+            return `${comment}${name}: {\n${fields.join("")}},\n`;
+        })
+        .join("");
+};
