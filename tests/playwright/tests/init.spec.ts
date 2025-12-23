baa598a095b3222a84d48335154e8aaaa1ca3527
Mori <139063007+mori2003@users.noreply.github.com>
2025-12-23 17:36:34 +0100

    wip

--- tests/playwright/tests/init.spec.ts
+++ tests/playwright/tests/init.spec.ts
@@ -1,35 +1,20 @@
 import path from "node:path";
 import { expect, test } from "@playwright/test";
 
-test("Init WebGL", async ({ page }) => {
-    await page.goto(`tests/playwright/tests/webgl.html`);
+const backends = [
+    { name: "WebGL", file: "webgl.html" },
+    { name: "WebGL2", file: "webgl2.html" },
+    { name: "WebGPU", file: "webgpu.html" },
+] as const;
 
-    expect(
-        await page.waitForEvent("console", {
-            predicate: (msg) => msg.text().includes("render complete"),
-            timeout: 5000,
-        }),
-    ).toBeTruthy();
-});
+backends.forEach((backend) => {
+    test(`Init ${backend.name}`, async ({ page }) => {
+        await page.goto(`tests/playwright/tests/${backend.file}`);
 
-test("Init WebGL2", async ({ page }) => {
-    await page.goto(`tests/playwright/tests/webgl2.html`);
-
-    expect(
-        await page.waitForEvent("console", {
-            predicate: (msg) => msg.text().includes("render complete"),
-            timeout: 5000,
-        }),
-    ).toBeTruthy();
-});
-
-test("Init WebGPU", async ({ page }) => {
-    await page.goto(`tests/playwright/tests/webgpu.html`);
-
-    expect(
-        await page.waitForEvent("console", {
-            predicate: (msg) => msg.text().includes("render complete"),
-            timeout: 5000,
-        }),
-    ).toBeTruthy();
+        expect(
+            await page.waitForEvent("console", {
+                predicate: (msg) => msg.text().includes("render complete"),
+            }),
+        ).toBeTruthy();
+    });
 });
